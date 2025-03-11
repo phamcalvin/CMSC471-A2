057e22a5fbfeb717f1847804e1cc7dd1974d2bc0
Calvin Pham <calvinpham430@gmail.com>
2025-03-11 17:58:08 -0400

    created a set to pull all unique types of crime, started next steps

--- js/script.js
+++ js/script.js
@@ -17,30 +17,31 @@ svg.append('g')
 let allData = [];
 //let xVar, yVar, sizeVar, targetYear;
 let xScale, yScale, sizeScale;
-
-
-
-
+let options = new Set()
 const t = 1000; // 1000ms = 1 second
 
 function init(){
-    d3.csv("./data/chicago_crime.csv", d => ({ 
+    d3.csv("./data/chicago_crime.csv", d => { 
     // this is the callback function, applied to each item in the array
-
         // Besides converting the types, we also simpilify the variable names here. 
-        primType: d["Primary Type"],
-        description: d.Description,
-        arrest: d.Arrest, // using + to convert to numbers; same below
-        domestic: d.Domestic, 
-        district: +d.District, 
-        ward: +d.Ward, 
-        year: +d.Year,
-       
-    }))
+        const crime = {
+            primType: d["Primary Type"],
+            description: d.Description,
+            arrest: d.Arrest, // using + to convert to numbers; same below
+            domestic: d.Domestic, 
+            district: +d.District, 
+            ward: +d.Ward, 
+            year: +d.Year
+        };
+        options.add(crime.primType); //Add a new type to our set (doesn't store dupes)
+
+        return crime;
+       })
     .then(data => {
+            console.log(options)
             console.log(data) // Check the structure in the console
             allData = data
-            
+            setupSelector()
             
             // Initial rendering steps:
             
@@ -49,3 +50,33 @@ function init(){
 }
 
 window.addEventListener('load', init);
+
+function setupSelector() {
+    d3.selectAll('.variable')
+        .each(function() {
+            d3.select(this).selectAll('myOptions')
+            .data([...options])
+            .enter()
+            .append('option')
+            .text(d => d)
+            .attr("value", d=>d)
+        })
+        .on("change", function (event) {
+            if(d3.select(this).property("id") === "") { //placeholder, replace with id of our input thingy
+                yVar = d3.select(this).property("value");
+            }
+            updateVis();
+            updateLegend(); //if we are implementing a checkbox system
+            //no need to change axes
+        });
+
+}
+
+function updateVis() {
+
+}
+
+//Update legend to match what options are selected
+function updateLegend() {
+
+}

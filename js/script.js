261ac67868a2b1bd1cd72c412a20ebc5bd30d07e
Calvin Pham <calvinpham430@gmail.com>
2025-03-12 13:41:08 -0400

    created legend and y axis functionality

--- js/script.js
+++ js/script.js
@@ -15,7 +15,11 @@ svg.append('g')
     .attr('transform', `translate(0,${height})`);
 
 let allData = [];
-
+let formatted;
+let colorScale;
+const line = d3.line()
+    .x(d => xScale(d.year))
+    .y(d => yScale(d.count));
 
 let xScale = d3.scaleLinear()
     .domain([2001, 2025])
@@ -24,8 +28,10 @@ let xScale = d3.scaleLinear()
 let yScale = d3.scaleLinear()
     .domain([0, 500000])
     .range([height, 0]);
+
 let options = new Set(["ALL"]);
 const crimeCounts = {};
+let yVar = "ALL" //maybe turn this into an array to read multiple values
 const t = 1000; // 1000ms = 1 second
 
 
@@ -55,57 +61,43 @@ function init(){
         crimeCounts[crime.year][crime.primType]++;
         crimeCounts[crime.year]["ALL"]++;
 
-
         return crime;
        })
     .then(data => {
             console.log(options)
             console.log(crimeCounts) // Check the structure in the console
+            formatted = Object.entries(crimeCounts).flatMap(([year, crimes]) => 
+                Object.entries(crimes).map(([primType, count]) => ({
+                    year: +year,
+                    primType,
+                    count
+                }))
+            );  //Format data to be passed into d3
+            console.log(formatted);
+
+            colorScale = d3.scaleOrdinal([...options],
+                ["#000000", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2",
+                "#7f7f7f", "#bcbd22", "#17becf", "#aec7e8", "#ffbb78", "#98df8a", "#ff9896",
+                "#c5b0d5", "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5", "#393b79",
+                "#637939", "#8c6d31", "#843c39", "#7b4173", "#5254a3", "#6b6ecf", "#b5cf6b",
+                "#e7ba52", "#e7969c", "#31a354", "#756bb1", "#636363", "#d6616b", "#ce6dbd",
+                "#e6550d"]
+                );
+            
+
             allData = data
+
             setupSelector()
-            
-            // Initial rendering steps: Set up fixed axes
-            
-
-            svg.append("g")
-                .attr("transform", `translate(0,${height})`)
-                .call(d3.axisBottom(xScale).ticks(12));
-            //axes lables
-            svg.append("text")
-                .attr("x", width / 2)
-                .attr("y", height + margin.bottom - 5)
-                .attr("text-anchor", "middle")
-                .text("Year") // Displays the current x-axis variable
-                .attr('class', 'labels');
-
-            
-            
-            svg.append("g")
-                .call(d3.axisLeft(yScale));
-            //axes labels THIS ONE IS A LIL WONKY
-            svg.append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("x", -height / 2)
-                .attr("y", -margin.left + 15)
-                .attr("text-anchor", "middle")
-                .text("Reports") // Displays the current y-axis variable
-                .attr('class', 'labels');
-            updateVis();
+            // Initial rendering steps:
+            updateAxes()
+            updateVis()
+            updateLegend()
         })
     .catch(error => console.error('Error loading data:', error));
 }
 
-window.addEventListener('load', init);
+window.addEventListener("load", init);
 //ChatGPT generated 37 distinct hexadecimal colors
-const colorScale = d3.scaleOrdinal([...options],
-    ["#ff7f0e", "#000000", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2",
-    "#7f7f7f", "#bcbd22", "#17becf", "#aec7e8", "#ffbb78", "#98df8a", "#ff9896",
-    "#c5b0d5", "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5", "#393b79",
-    "#637939", "#8c6d31", "#843c39", "#7b4173", "#5254a3", "#6b6ecf", "#b5cf6b",
-    "#e7ba52", "#e7969c", "#31a354", "#756bb1", "#636363", "#d6616b", "#ce6dbd",
-    "#e6550d"]
-    );
-
 
 function setupSelector() {
     d3.selectAll('.variable')
@@ -124,34 +116,59 @@ function setupSelector() {
             if(d3.select(this).property("id") === "") { //placeholder, replace with id of our input thingy
                 yVar = d3.select(this).property("value");
             }
-            
+            updateAxes();
+            updateVis();
             updateLegend();
         });
 
 }
 
-const line = d3.line()
-    .x(d => xScale(d.year))
-    .y(d => yScale(d.count));
+function updateAxes() {
+    svg.selectAll('.axis').remove();
+    svg.selectAll('.labels').remove();
 
+    //fixed x axis
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(xScale).ticks(12))
+        .attr("class", "axis");
+
+    //axes lables
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 5)
+        .attr("text-anchor", "middle")
+        .text("Year") // Displays the current x-axis variable
+        .attr('class', 'labels');
+
+    //Change y axis scale from 500k to 100k if all is not selected
+    yScale = d3.scaleLinear()
+        .domain([0, yVar === "ALL" ? 500000 : 100000])
+        .range([height, 0]);
+    
+    const yAxis = d3.axisLeft(yScale);
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(0,0)`)
+        .call(yAxis);
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 15)
+        .attr("text-anchor", "middle")
+        .text("Reports") 
+        .attr('class', 'labels');
+}
 
 function updateVis() {
-    const formatted = Object.entries(crimeCounts).flatMap(([year, crimes]) => 
-        Object.entries(crimes).map(([primType, count]) => ({
-            year: +year,
-            primType,
-            count
-        }))
-    );
-    
-    console.log(formatted);
-
-
-    [...options].forEach((crime, i) => {
+    //Iterate through each crime type in the list and draw a line
+    //TODO iterate through crimes selected
+    [...options].forEach((crime) => {
         svg.append("path")
         .datum(formatted.filter(d => d.primType === crime))
         .attr("fill", "none")
-        .attr("stroke", colorScale(i))
+        .attr("stroke", colorScale(crime))
         .attr("stroke-width", 2)
         .attr("d", line)
     });
@@ -159,5 +176,26 @@ function updateVis() {
 
 //Update legend to match what options are selected
 function updateLegend() {
+    let size = 10;
+    svg.selectAll('crimeSquare')
+        .data([...options])
+        .enter()
+        .append("rect")
+        .attr("y", (d, i) => i * (size + 10) - 30)
+        .attr("x", 600)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", d => colorScale(d))
 
+    
+    svg.selectAll('crimeName')
+        .data([...options])
+        .enter()
+        .append("text")
+        .attr("y", (d, i) => i * (size + 10) + size - 30)
+        .attr("x", 600 + size)
+        .style("fill", d => colorScale(d))
+        .text(d => d)
+        .attr("text-anchor", "left")
+        .style("font-size", "13px")
 }
